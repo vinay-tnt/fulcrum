d7bfee8e3fa68d8eb0a53bb4e0e38f4e4facd436
Vinay Uttam Vemparala <vinay.vemparala@tagntrac.com>
2025-01-06 14:44:36 +0530

    change CDN assets path

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,9 @@ module Fulcrum
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    # Custom path for asset pipeline
+    config.assets.prefix = '/rp-assets'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
